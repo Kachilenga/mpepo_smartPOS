ccd77f98333041370917aa3c22e3328021f800fe
Kachilenga <kachilengabanda@gmail.com>
2025-09-07 22:43:35 +0200

    first code pushed with edit

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You havent pushed the button this many times:'),
+            const Text('You havent pushed the button this many times have you:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
